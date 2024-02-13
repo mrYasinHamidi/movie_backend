03129b6262d13719a07784e330223513b775c6bd
Yasin <yasinhamidi50@gmail.com>
2024-02-13 07:05:19 -0800

    inject auth service in root

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,8 +1,13 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:movie_backend/services/authentication_service.dart';
 import 'package:movie_backend/services/database_service.dart';
 
 Handler middleware(Handler handler) {
-  return handler.use(
-    DatabaseService.provide(),
-  );
+  return handler
+      .use(
+        AuthenticationService.provide(),
+      )
+      .use(
+        DatabaseService.provide(),
+      );
 }
--- routes/auth/_middleware.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_frog/dart_frog.dart';
-import 'package:movie_backend/services/authentication_service.dart';
-
-Handler middleware(Handler handler) {
-  return handler.use(
-    AuthenticationService.provide(),
-  );
-}
